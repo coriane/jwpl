9ba11eff0a78e6b02935714e6fd4f3b1973ed192
oliver.ferschke <oliver.ferschke@10d8a5b4-7c7b-defa-3ec6-669fcde35942>
2012-05-29 09:18:50 +0000

    Removed getInlinkAnchors() and getOutlinkAnchors(). Moved to de.tudarmstadt.ukp.wikipedia.parser.LinkAnchorExtractor

--- trunk/de.tudarmstadt.ukp.wikipedia.api/src/main/java/de/tudarmstadt/ukp/wikipedia/api/Page.java
+++ trunk/de.tudarmstadt.ukp.wikipedia.api/src/main/java/de/tudarmstadt/ukp/wikipedia/api/Page.java
@@ -618,111 +618,15 @@ public class Page
 		return cp;
 	}
 
+
+	///////////////////////////////////////////////////////////////////
 	/*
-	 * The methods getInlinkAnchors() and getOutLinkAnchors() have to be migrated to the SWEBLE parser or the code has to be
-	 * moved to the parser.
+	 * The methods getInlinkAnchors() and getOutLinkAnchors() not yet been
+	 * migrated to the SWEBLE parser. The original versions based on the
+	 * JWPL MediaWikiParser can be found in
+	 * de.tudarmstadt.ukp.wikipedia.parser.LinkAnchorExtractor
 	 */
-
-
-//	/**
-//()) {
-			ParsedPage pp = p.getParsedPage();
-			if (pp == null) {
-				return ipage's title.
-//)) {
-				String pageTitle = hibernatePage.getName();
-
-				String anchorText = l.getText();
-			// *
-	 * @return A ParsedPage object as would have been returned by the JWPL Parser.
-	 */
-	public ParsedPage getParsedPag//	 *
-//	{
-		return wiki.getParser().parse(getText());
-	}
-
-	/**
-	 * Note that this method //	 * @throws WikiTitleParsingException
-//	 */
-//	public Set<String> getInlinkAnchors()
-//		throws WikiTitleParsingException
-//	{
-//		Set<String> inAnchors = new HashSet<String>();
-//		for (Page p : getInlinks()) {
-//			ParsedPage pp = p.getParsedPage();
-//			if (pp == null) {
-//				return inAnchors;
-//			}
-//			for (Link l : pp.getLinks()) {
-//				String pageTitle = hibernatePage.getName();
-//
-//				String anchorText = l.getText();
-//				if (l.getTarget().equals(pageTitle) && !anchorText.equals(pageTitle)) {
-//					inAnchors.add(anchorText);
-//				}
-//			}
-//		}
-//		return inAnchors;
-//	}
-//
-//	/**
-//()) {
-			ParsedPage pp = p.getParsedPage();
-			if (pp == null) {
-				return inAnchors;
-			}
-			//	 * they are pointing to.
-//)) {
-				String pageTitle = hibernatePage.getName();
-
-				String anchorText = l.getText();
-			// *
-	 * @return A ParsedPage object as would have been returned by the JWPL Parser.
-	 */
-	public ParsedPage getParsedPag//	 *
-//	 * @return A mapping from the page titles of links in that page to the anchor texts used in the
-//	 *         links.
-//	 * @throws WikiTitleParsingException
-//	 */
-//	public Map<String, Set<String>> getOutlinkAnchors()
-//		throws WikiTitleParsingException
-//	{
-//		Map<String, Set<String>> outAnchors = new HashMap<String, Set<String>>();
-//		ParsedPage pp = getParsedPage();
-//		if (pp == null) {
-//			return outAnchors;
-//		}
-//		for (Link l : pp.getLinks()) {
-//			if (l.getTarget().length() == 0) {
-//				continue;
-//			}
-//
-//tleParsingException
-	 */
-	public Map<String, Set<String>> getOutli//			if (!l.getType().equals(Link.type.EXTERNAL) && !l.getType().equals(Link.type.IMAGE)
-//					&& !l.getType().equals(Link.type.AUDIO) && !l.getType().equals(Link.type.VIDEO)
-// null) {
-			return outAnchors;
-		}
-		for (Link l : pp.getLinks()) {
-			if (l.getTarg//													// are categories or other meta data
-//			{
-//				String anchorText = l.getText();
-//				if (!anchorText.equals(targetTitle)) {
-//					Set<String> anchors;
-//					if (outAnchors.containsKey(targetTitle)) {
-//						anchors = outAnchors.get(targetTitle);
-//					}
-//					else {
-//						anchors = new HashSet<String>();
-//					}
-//					anchors.add(anchorText);
-//					outAnchors.put(targetTitle, anchors);
-//				}
-//			}
-//		}
-//		return outAnchors;
-//g anchorText = l.getText();
+	///////////////////////////////////////////////////////////////////nchorText = l.getText();
 				if (!anchorText.equals(targetTitle)) {
 					Set<String> anchors;
 					if (outAnchors.containsKey(targetTitle)) {
